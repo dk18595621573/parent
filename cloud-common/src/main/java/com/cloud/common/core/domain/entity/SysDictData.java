e19c023902f22522cc9974d669eac2b463342b64
曾浩 <epdcgsi@dingtalk.com>
2022-06-14 15:32:38 +0800

    代码调整

--- cloud-common/src/main/java/com/cloud/common/core/domain/entity/SysDictData.java
+++ cloud-common/src/main/java/com/cloud/common/core/domain/entity/SysDictData.java
@@ -136,7 +136,7 @@ public class SysDictData extends BaseEntity {
 
     @JsonIgnore
     public boolean getDefault() {
-        return UserConstants.YES.equals(this.isDefault) ? true : false;
+        return UserConstants.YES.equals(this.isDefault);
     }
 
     public String getIsDefault() {
--- cloud-common/src/main/java/com/cloud/common/enums/RuleRange.java
+++ cloud-common/src/main/java/com/cloud/common/enums/RuleRange.java
@@ -4,9 +4,9 @@ package com.cloud.common.enums;
  * 规则范围
  */
 public enum RuleRange {
-    SAME_TYPE__ORDER(1l, "同类型订单"),
-    ONLY_THIS(2l, "仅此订单"),
-    ALL_TYPES(3l, "所有类型");
+    SAME_TYPE__ORDER(1L, "同类型订单"),
+    ONLY_THIS(2L, "仅此订单"),
+    ALL_TYPES(3L, "所有类型");
 
 
     private final Long code;
