7255ced760204732596761433039f08deadc6bd6
曾浩 <epdcgsi@dingtalk.com>
2022-06-28 15:13:15 +0800

    系统参数配置注释

--- cloud-common/src/main/java/com/cloud/common/enums/SystemParams.java
+++ cloud-common/src/main/java/com/cloud/common/enums/SystemParams.java
@@ -13,27 +13,27 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SystemParams {
     /**
-     * 系统初始密码
+     * 系统初始密码 默认：123456
      */
     INIT_PASSWORD("sys.user.initPassword", "123456"),
     /**
-     * 验证码开关
+     * 验证码开关 默认:打开验证码
      */
     CAPTCHA_ENABLE("sys.account.captchaOnOff", "true"),
     /**
-     * 注册开关
+     * 注册开关 默认：不允许注册
      */
     REGISTER_ENABLE("sys.account.registerUser", "false"),
     /**
-     * 模拟支付开关
+     * 模拟支付开关 默认：不允许模拟支付
      */
     SIMULATE_CONFIG("pay.channel.simulate", "false"),
     /**
-     * 默认代发仓编码
+     * 默认代发仓编码 默认：醒市测试仓
      */
     DEFAULT_WAREHOUSE("sys.warehouse.code", "xscec"),
     /**
-     * 是否可以注册为需求方
+     * 是否可以注册为需求方 默认：不支持注册需求方
      */
     DEMAND_REGISTER("user.company.demand", "false"),
     ;
