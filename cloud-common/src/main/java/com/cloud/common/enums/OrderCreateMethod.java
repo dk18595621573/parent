ef8bcd2b068cecd6758fcb89da0346aa18c96065
zhouyong <1198113391@qq.com>
2022-07-04 18:18:04 +0800

    枚举

---
+++ cloud-common/src/main/java/com/cloud/common/enums/OrderCreateMethod.java
@@ -0,0 +1,42 @@
+package com.cloud.common.enums;
+
+import java.util.Objects;
+
+/**
+ * 订单创建方式（1. 界面新建；2. Excel导入；3.erp拉取）
+ */
+public enum OrderCreateMethod {
+    NEW_INTERFACE(1, "界面新建"),
+    EXCEL_IMPORT(2, "Excel导入"),
+    ERP_PULL(3, "erp拉取"),
+    ;
+
+
+    private final Integer code;
+
+    private final String msg;
+
+    OrderCreateMethod(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public static OrderCreateMethod fromCode(Integer code) {
+        if (Objects.nonNull(code)) {
+            for (OrderCreateMethod status : OrderCreateMethod.values()) {
+                if (status.getCode().equals(code)) {
+                    return status;
+                }
+            }
+        }
+        return null;
+    }
+}
