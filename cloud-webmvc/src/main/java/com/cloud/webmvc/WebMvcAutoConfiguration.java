5f48492eb241fe937c7a1c7bd014efb8006ca6b4
曾浩 <epdcgsi@dingtalk.com>
2022-07-05 13:28:57 +0800

    webmvc项目调整

--- cloud-webmvc/src/main/java/com/cloud/webmvc/WebMvcAutoConfiguration.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/WebMvcAutoConfiguration.java
@@ -1,21 +1,10 @@
 package com.cloud.webmvc;
 
-import com.cloud.webmvc.aspect.LogAspect;
-import com.cloud.webmvc.aspect.RateLimiterAspect;
-import com.cloud.webmvc.config.ApplicationConfig;
-import com.cloud.webmvc.config.CaptchaConfig;
-import com.cloud.webmvc.config.ResourcesConfig;
-import com.cloud.webmvc.config.SecurityConfig;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
 /**
  * webmvc自动装配.
  *
  * @author zenghao
  * @date 2022/7/5
  */
-@Configuration
-@Import({ApplicationConfig.class, CaptchaConfig.class, ResourcesConfig.class, SecurityConfig.class, LogAspect.class, RateLimiterAspect.class})
 public class WebMvcAutoConfiguration {
 }
