8953544695086780296bbc86bfa8902bb6519a84
Mft <2664370462@qq.com>
2022-07-08 13:08:54 +0800

    整理枚举

---
+++ cloud-common/src/main/java/com/cloud/common/enums/ChinaPayBank.java
@@ -0,0 +1,77 @@
+package com.cloud.common.enums;
+
+/**
+ * @author mft
+ * <p>
+ * 银联打款
+ */
+
+public enum ChinaPayBank {
+
+    /**
+     * 中国银行
+     */
+    B_O_C(5, "中国银行"),
+
+    /**
+     * 中国建设银行
+     */
+    C_C_B(6, "中国建设银行"),
+
+    /**
+     * 中国工商银行
+     */
+    I_C_B_C(7, "中国工商银行"),
+
+    /**
+     * 中国交通银行
+     */
+    B_C_M(8, "中国交通银行"),
+
+    /**
+     * 中国农业银行
+     */
+    A_B_C(9, "中国农业银行"),
+
+    /**
+     * 中国邮政储蓄银行
+     */
+    P_S_B_C(10, "中国邮政储蓄银行"),
+
+    /**
+     * 招商银行
+     */
+    C_M_B(11, "招商银行"),
+
+    /**
+     * 兴业银行
+     */
+    C_I_B(12, "兴业银行"),
+
+    /**
+     * 浦发银行
+     */
+    S_P_D_B(13, "浦发银行"),
+
+    /**
+     * 平安银行
+     */
+    P_A_B(14, "平安银行");
+
+    private final Integer code;
+
+    private final String msg;
+
+    ChinaPayBank(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
---
+++ cloud-common/src/main/java/com/cloud/common/enums/ChinaPayType.java
@@ -0,0 +1,52 @@
+package com.cloud.common.enums;
+
+/**
+ * @author mft
+ * <p>
+ * 银联打款
+ */
+
+public enum ChinaPayType {
+
+    /**
+     * 打款银行账号
+     */
+    PAY_BANK_ACCOUNT(0, "215500700"),
+
+    /**
+     * 打款开户银行
+     */
+    PAY_ACCOUNT_BANK(0, "上海银联电子支付服务有限公司"),
+
+    /**
+     * 银联打款状态
+     */
+    ORDER_STATUS(0, "0000"),
+
+    /**
+     * 银联企业代码类型 1：统一信用代码 3：工商注册号
+     */
+    KEY_TYPE_CREDIT(1, "1"),
+
+    /**
+     * 银联企业代码类型 1：统一信用代码 3：工商注册号
+     */
+    KEY_TYPE(3, "3");
+
+    private final Integer code;
+
+    private final String msg;
+
+    ChinaPayType(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
