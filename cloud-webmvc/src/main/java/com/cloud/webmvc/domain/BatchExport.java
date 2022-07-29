f3e15831bce4e6d5cf6f10b6dba18517882ea862
曾浩 <epdcgsi@dingtalk.com>
2022-07-29 09:53:41 +0800

    文件导出通用对象

--- cloud-webmvc/src/main/java/com/cloud/webmvc/domain/BatchExport.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/domain/BatchExport.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
  * @date 2022/7/29
  */
 @Data
-@ApiModel("批量导出VO")
+@ApiModel(value = "批量导出VO")
 public class BatchExport implements Serializable {
 
     @ApiModelProperty("导出批次号")
@@ -21,4 +21,15 @@ public class BatchExport implements Serializable {
 
     @ApiModelProperty("导出文件地址")
     private String filepath;
+
+    public static BatchExport of(final String batchNo) {
+        return of(batchNo, null);
+    }
+
+    public static BatchExport of(final String batchNo, final String filepath) {
+        BatchExport export = new BatchExport();
+        export.setBatchNo(batchNo);
+        export.setFilepath(filepath);
+        return export;
+    }
 }
