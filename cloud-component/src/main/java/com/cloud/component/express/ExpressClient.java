107eac9336a20dfdbf9b2fd1ed95b2ea3db6a285
XieHaiTao <m13122917937@163.com>
2022-07-31 14:46:21 +0800

    快递接口调整

--- cloud-component/src/main/java/com/cloud/component/express/ExpressClient.java
+++ cloud-component/src/main/java/com/cloud/component/express/ExpressClient.java
@@ -1,27 +1,21 @@
 package com.cloud.component.express;
 
 import cn.hutool.core.convert.ConvertException;
-import cn.hutool.core.lang.TypeReference;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.ContentType;
 import cn.hutool.http.Header;
 import cn.hutool.http.HttpException;
 import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
-import cn.hutool.http.HttpUtil;
-import cn.hutool.json.JSONUtil;
 import com.cloud.common.exception.ServiceException;
 import com.cloud.common.utils.json.JsonUtil;
 import com.cloud.component.express.consts.ErrorCode;
-import com.cloud.component.express.domain.JuheExpress;
-import com.cloud.component.express.domain.JuheResult;
-import com.cloud.component.express.domain.KuaiDi100Result;
+import com.cloud.component.express.domain.ExpressError;
+import com.cloud.component.express.domain.ExpressResult;
 import com.cloud.component.express.exception.ExpressException;
 import com.cloud.component.properties.ExpressProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -50,11 +44,11 @@ public class ExpressClient {
      * 快递100 查询快递信息接口
      *
      * @param expressCode
-     * @param expressNo
+     * @param expressNo1
      * @param cellphone
      * @return
      */
-    public KuaiDi100Result findExpress2(final String expressCode, final String expressNo, final String cellphone) {
+    public ExpressResult findExpress2(final String expressCode, final String expressNo, final String cellphone) {
         HashMap paramMap = new HashMap<>() {{
             // 快递公司
             put("com", expressCode);
@@ -70,7 +64,7 @@ public class ExpressClient {
             put("sign", sign(param + expressProperties.getKey() + expressProperties.getCustomer()));
             put("param", param);
         }};
-        KuaiDi100Result kuaiDi100Result = null;
+        ExpressResult expressResult = null;
         try {
             String result = HttpRequest.post(expressProperties.getUrl()).header(Header.CONTENT_TYPE, ContentType.FORM_URLENCODED.getValue())
                 .timeout(TIMEOUT).form(requestBody).execute().body();
@@ -79,7 +73,14 @@ public class ExpressClient {
                 throw new ExpressException(ErrorCode.API_ERROR);
             }
             // 转换 对象
-            kuaiDi100Result = JsonUtil.parse(result, KuaiDi100Result.class);
+            ExpressError expressError = JsonUtil.parse(result, ExpressError.class);
+            if (StrUtil.isNotBlank(expressError.getReturnCode()) ) {
+                if (Objects.equals(ExpressError.QUERY_ERROR_CODE, expressError.getReturnCode())) {
+                    throw new ExpressException(ErrorCode.NOT_FOUND);
+                }
+                throw new ServiceException(expressError.getMessage());
+            }
+            expressResult = JsonUtil.parse(result, ExpressResult.class);
 
         } catch (HttpException e) {
             log.warn("调用快递API接口异常[{}]:{}", expressNo, e.getMessage());
@@ -88,17 +89,8 @@ public class ExpressClient {
             log.warn("调用快递API返回单号[{}]响应的快递数据转换异常:{}", expressNo, e);
             throw new ExpressException(ErrorCode.API_ERROR);
         }
-        if (Objects.isNull(kuaiDi100Result) || !Objects.equals(kuaiDi100Result.getStatus(), KuaiDi100Result.SUCCESS_CODE)) {
-            if (Objects.equals(KuaiDi100Result.QUERY_ERROR, kuaiDi100Result.getReturnCode())) {
-                throw new ExpressException(ErrorCode.NOT_FOUND);
-            } else if (Objects.equals(KuaiDi100Result.KEY_ERROR, kuaiDi100Result.getReturnCode())) {
-                throw new ServiceException(kuaiDi100Result.getMessage());
-            } else {
-                throw new ExpressException(ErrorCode.QUERY_ERROR);
-            }
-        }
         //查询失败状态处理
-        return kuaiDi100Result;
+        return expressResult;
     }
 
     /**
@@ -111,4 +103,14 @@ public class ExpressClient {
         return DigestUtils.md5Hex(msg).toUpperCase();
     }
 
+    public static void main(String[] args) {
+        ExpressProperties properties = new ExpressProperties();
+        properties.setUrl("https://poll.kuaidi100.com/poll/query.do");
+        properties.setKey("wBfjOuYf6894");
+        properties.setCustomer("C7AF17641A07E84D1C93C43645515C69");
+
+        ExpressClient expressClient = new ExpressClient(properties);
+        ExpressResult express2 = expressClient.findExpress2("shunfeng", "SF1120093426982", "19120872793");
+        System.out.println(express2);
+    }
 }
--- cloud-component/src/main/java/com/cloud/component/express/domain/ExpressResult.java
+++ cloud-component/src/main/java/com/cloud/component/express/domain/ExpressResult.java
@@ -14,7 +14,7 @@ import java.util.List;
  * @website http://www.bejson.com/java2pojo/
  */
 @Data
-public class KuaiDi100Result {
+public class ExpressResult {
 
     public static final String SUCCESS_CODE = "200";
 
@@ -23,39 +23,14 @@ public class KuaiDi100Result {
     public static final String KEY_ERROR = "601";
 
     /**
-     * 异常状态吗.
-     */
-    private String returnCode;
-
-    /**
-     * 消息体，请忽略
-     */
-    private String message;
-
-    /**
      * 单号
      */
     private String nu;
 
     /**
-     * 是否签收标记，0未签收，1已签收，请忽略，明细状态请参考state字段
-     */
-    private String ischeck;
-
-    /**
-     * 快递单明细状态标记，暂未实现，请忽略
-     */
-    private String condition;
-
-    /**
      * 快递公司编码,一律用小写字母
      */
     private String com;
-
-    /**
-     * 通讯状态，请忽略
-     */
-    private String status;
 
     /**
      * 快递单当前状态，默认为0在途，1揽收，2疑难，3签收，4退签，5派件，8清关，14拒签等10个基础物流状态，如需要返回高级物流状态，请参考 resultv2 传值
@@ -65,11 +40,11 @@ public class KuaiDi100Result {
     /**
      * 最新查询结果，数组，包含多项，全量，倒序（即时间最新的在最前），每项都是对象，对象包含字段请展开
      */
-    private List<KuaiDi100Express> data;
+    private List<ExpressItem> data;
 
 
     @Data
-    public static class KuaiDi100Express {
+    public static class ExpressItem {
 
         /**
          * 格式化后时间.
@@ -80,7 +55,6 @@ public class KuaiDi100Result {
          * 内容.
          */
         private String context;
-
 
     }
 }
