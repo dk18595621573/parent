36762caed4588405266a400fd946956f10fa1bac
曾浩 <epdcgsi@dingtalk.com>
2022-07-31 13:42:44 +0800

    代码调整

--- cloud-webmvc/src/main/java/com/cloud/webmvc/excel/RowMergeStrategy.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/excel/RowMergeStrategy.java
@@ -11,6 +11,7 @@ import org.apache.poi.ss.util.CellRangeAddress;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * 自定义行合并策略.
@@ -50,37 +51,34 @@ public class RowMergeStrategy extends AbstractMergeStrategy {
 
     /**
      * 合并行
-     * @param sheet 当前sheet
-     * @param cell 合并基准数据（可以指定根据某列数据合并，也可以根据当前单元格和上一个单元格数据自动合并）
+     *
+     * @param sheet       当前sheet
+     * @param cell        合并基准数据（可以指定根据某列数据合并，也可以根据当前单元格和上一个单元格数据自动合并）
      * @param curRowIndex 当前单元格行索引
      * @param curColIndex 当前单元格列索引
      */
     private void mergeWithPrevRow(Sheet sheet, Cell cell, int curRowIndex, int curColIndex) {
         //获取当前行的当前列的数据和上一行的当前列列数据，通过上一行数据是否相同进行合并
         Object curData = cell.getCellType() == CellType.STRING ? cell.getStringCellValue() : cell.getNumericCellValue();
-        Row preRow = cell.getSheet().getRow(curRowIndex - 1);
+        int preRowIndex = curRowIndex - 1;
+        Row preRow = cell.getSheet().getRow(preRowIndex);
         Cell preCell = preRow.getCell(cell.getColumnIndex());
         Object preData = preCell.getCellType() == CellType.STRING ? preCell.getStringCellValue() : preCell.getNumericCellValue();
         // 比较当前行的第一列的单元格与上一行是否相同，相同合并当前单元格与上一行
         if (Objects.equals(curData, preData)) {
             List<CellRangeAddress> mergeRegions = sheet.getMergedRegions();
-            boolean isMerged = false;
-            for (int i = 0; i < mergeRegions.size() && !isMerged; i++) {
-                CellRangeAddress cellRangeAddr = mergeRegions.get(i);
-                // 若上一个单元格已经被合并，则先移出原有的合并单元，再重新添加合并单元
-                if (cellRangeAddr.isInRange(curRowIndex - 1, curColIndex)) {
-                    sheet.removeMergedRegion(i);
-                    cellRangeAddr.setLastRow(curRowIndex);
-                    sheet.addMergedRegion(cellRangeAddr);
-                    isMerged = true;
-                }
+            Optional<CellRangeAddress> optional = mergeRegions.stream()
+                .filter(c -> c.isInRange(preRowIndex, curColIndex)).findFirst();
+
+            CellRangeAddress address;
+            if (optional.isPresent()) {
+                address = optional.get();
+                sheet.removeMergedRegion(mergeRegions.indexOf(address));
+                address.setLastRow(curRowIndex);
+            } else {
+                address = new CellRangeAddress(preRowIndex, curRowIndex, curColIndex, curColIndex);
             }
-            // 若上一个单元格未被合并，则新增合并单元
-            if (!isMerged) {
-                CellRangeAddress cellRangeAddress = new CellRangeAddress(curRowIndex - 1, curRowIndex, curColIndex,
-                    curColIndex);
-                sheet.addMergedRegion(cellRangeAddress);
-            }
+            sheet.addMergedRegion(address);
         }
     }
 }
