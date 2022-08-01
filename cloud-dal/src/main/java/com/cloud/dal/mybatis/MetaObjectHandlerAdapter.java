90f525616510818bbd45119c7e3bd7fc2ce16d51
曾浩 <epdcgsi@dingtalk.com>
2022-08-01 19:02:05 +0800

    mybatis plus自动填充调整

--- cloud-dal/src/main/java/com/cloud/dal/mybatis/MetaObjectHandlerAdapter.java
+++ cloud-dal/src/main/java/com/cloud/dal/mybatis/MetaObjectHandlerAdapter.java
@@ -2,11 +2,11 @@ package com.cloud.dal.mybatis;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.core.handlers.StrictFill;
-import com.cloud.dal.model.BaseEntity;
-import com.cloud.common.exception.ServiceException;
-import com.cloud.common.utils.DateUtils;
 import com.cloud.common.threads.RequestThread;
+import com.cloud.common.utils.DateUtils;
+import com.cloud.dal.model.BaseEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.ibatis.reflection.MetaObject;
 
 import java.util.ArrayList;
@@ -29,16 +29,9 @@ public class MetaObjectHandlerAdapter implements MetaObjectHandler {
     @Override
     public void insertFill(final MetaObject metaObject) {
         if (metaObject.getOriginalObject() instanceof BaseEntity) {
-            Long operatorId;
-            try {
-                operatorId = RequestThread.getUserId();
-            } catch (ServiceException e) {
-                log.warn("当前新增记录自动填充时无法获取登录用户信息");
-                operatorId = 0L;
-            }
+            Long operatorId = ObjectUtils.defaultIfNull(RequestThread.getUserId(), 0L);
 
             Date now = DateUtils.getNowDate();
-
             List<StrictFill<?, ?>> list = new ArrayList<>(4);
             list.add(StrictFill.of("createBy", Long.class, operatorId));
             list.add(StrictFill.of("updateBy", Long.class, operatorId));
@@ -56,13 +49,8 @@ public class MetaObjectHandlerAdapter implements MetaObjectHandler {
     @Override
     public void updateFill(final MetaObject metaObject) {
         if (metaObject.getOriginalObject() instanceof BaseEntity) {
-            Long operatorId;
-            try {
-                operatorId = RequestThread.getUserId();
-            } catch (ServiceException e) {
-                log.warn("当前修改记录自动填充时无法获取登录用户信息");
-                operatorId = 0L;
-            }
+            Long operatorId = ObjectUtils.defaultIfNull(RequestThread.getUserId(), 0L);
+
             List<StrictFill<?, ?>> list = new ArrayList<>(2);
             list.add(StrictFill.of("updateBy", Long.class, operatorId));
             list.add(StrictFill.of("updateTime", Date.class, DateUtils.getNowDate()));
