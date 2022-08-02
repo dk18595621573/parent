4f8c059cd12995464f0f2410cdbb361fb91b1477
zhouyong <1198113391@qq.com>
2022-08-02 11:36:21 +0800

    easy Excel过滤空行数据

--- cloud-webmvc/src/main/java/com/cloud/webmvc/excel/listener/ProcessAllListener.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/excel/listener/ProcessAllListener.java
@@ -1,10 +1,12 @@
 package com.cloud.webmvc.excel.listener;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.cloud.common.utils.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.function.Function;
 
 /**
@@ -27,6 +29,23 @@ public class ProcessAllListener<T> extends AnalysisEventListener<T> {
 
     @Override
     public void invoke(final T data, final AnalysisContext context) {
+        // 过滤全是空的数据
+        boolean exist = false;
+        String[] fieldName = getFieldName(data);
+        for (String string : fieldName) {
+            Object fieldValue = getFieldValue(data, string);
+            if (fieldValue instanceof String) {
+                if (StringUtils.isNotBlank((String) fieldValue)) {
+                    exist = true;
+                }
+            }
+            if (!Objects.isNull(fieldValue)) {
+                exist = true;
+            }
+        }
+        if (!exist) {
+            return;
+        }
         dataList.add(data);
     }
 
@@ -37,5 +56,46 @@ public class ProcessAllListener<T> extends AnalysisEventListener<T> {
 
     public String getMessage(){
         return message;
+    }
+
+    /**
+     * 获取属性名数组
+     */
+    public static String[] getFieldName(Object o) {
+        Field[] fields = o.getClass().getDeclaredFields();
+        List<String> fieldNames = new LinkedList<>();
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i].isAnnotationPresent(ExcelProperty.class)) {
+                fieldNames.add(fields[i].getName());
+            }
+        }
+        return fieldNames.toArray(String[]::new);
+    }
+
+    /**
+     * 通过属性名获取属性值  忽略大小写
+     *
+     * @param o
+     * @param name
+     * @return
+     * @throws Exception
+     */
+
+    public static Object getFieldValue(Object o, String name) {
+        try {
+            Field[] fields = o.getClass().getDeclaredFields();
+            Object object = null;
+            for (Field field : fields) {
+                // 可以获取到私有属性
+                field.setAccessible(true);
+                if (field.getName().toUpperCase().equals(name.toUpperCase())) {
+                    object = field.get(o);
+                    break;
+                }
+            }
+            return object;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
