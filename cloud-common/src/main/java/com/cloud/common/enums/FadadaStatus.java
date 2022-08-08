2bc15661278fb862fb6fdcb7a5e28108d4e4de66
Mft <2664370462@qq.com>
2022-08-08 18:54:26 +0800

    法大大返回信息枚举

---
+++ cloud-common/src/main/java/com/cloud/common/enums/FadadaStatus.java
@@ -0,0 +1,43 @@
+package com.cloud.common.enums;
+
+/**
+ * @author mft
+ * <p>
+ * 法大大
+ */
+
+public enum FadadaStatus {
+
+    /**
+     * 签署回调code
+     */
+    EXTSIGN_CALLBACK_CODE(0, "3000"),
+
+    /**
+     * 实名认证回调
+     */
+    VERIFY_CALLBACK_FOUR(4, "4"),
+
+    /**
+     * 合同归档code
+     */
+    CONTRACT_FILING_CODE(1000, "1000");
+
+
+    private final Integer code;
+
+    private final String msg;
+
+    FadadaStatus(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
