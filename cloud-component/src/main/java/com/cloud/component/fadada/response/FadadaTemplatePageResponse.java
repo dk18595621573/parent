4738f8ba85ba9af21fd08f0a206dc4006b16c561
Mft <2664370462@qq.com>
2022-08-16 19:05:28 +0800

    法大大多方签署

---
+++ cloud-component/src/main/java/com/cloud/component/fadada/response/FadadaTemplatePageResponse.java
@@ -0,0 +1,18 @@
+package com.cloud.component.fadada.response;
+
+import lombok.Data;
+
+/**
+ * 响应信息
+ *
+ * @author mft
+ */
+@Data
+public class FadadaTemplatePageResponse extends FadadaBaseResponse {
+
+    /**
+     * 返回信息
+     * 非必填
+     */
+    private FadadaAddSignatureResponse data;
+}
