8d84c4f9bc70573acd28815b964b5e5965b0574c
zhouyong <1198113391@qq.com>
2022-08-19 13:18:39 +0800

    抢单时间bug

--- cloud-common/src/main/java/com/cloud/common/core/message/DemandOrder.java
+++ cloud-common/src/main/java/com/cloud/common/core/message/DemandOrder.java
@@ -229,6 +229,7 @@ public class DemandOrder implements Serializable {
         /**
          * 最后一次抢单时间
          */
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         private Date lastCompeteTime;
 
         /**
