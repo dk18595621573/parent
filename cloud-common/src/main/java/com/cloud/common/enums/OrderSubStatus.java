53431d62955c22e1b7b4f97199f4bea2a85a637a
曾浩 <epdcgsi@dingtalk.com>
2022-09-15 13:57:25 +0800

    报价中增加子状态

--- cloud-common/src/main/java/com/cloud/common/enums/OrderSubStatus.java
+++ cloud-common/src/main/java/com/cloud/common/enums/OrderSubStatus.java
@@ -28,7 +28,9 @@ public enum OrderSubStatus {
     /** 待抢单 */
     PENDING_ORDER(30, "待抢单"),
     /** 待确认 */
-    TO_BE_CONFIRMED(31, "待确认"),
+    TO_BE_CONFIRMED(31, "抢单待确认"),
+    /** 稍后发布 */
+    LATER_RELEASE(32, "稍后发布"),
 
     /** 目前需要提交串码 */
     TO_BE_COMPLETE(40, "待完善发货信息"),
