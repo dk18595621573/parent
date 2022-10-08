355b9b84e9c6c4bf88daf164c42a23b4bec03461
曾浩 <epdcgsi@dingtalk.com>
2022-10-08 18:43:13 +0800

    订单子状态调整

--- cloud-common/src/main/java/com/cloud/common/enums/OrderSubStatus.java
+++ cloud-common/src/main/java/com/cloud/common/enums/OrderSubStatus.java
@@ -33,6 +33,8 @@ public enum OrderSubStatus {
     TO_BE_CONFIRMED(31, "抢单待确认"),
     /** 稍后发布 */
     LATER_RELEASE(32, "稍后发布"),
+    /** 稍后发布已抢单 */
+    LATER_RELEASE_TRADE(33, "稍后发布已抢单"),
 
     /** 目前需要提交串码 */
     TO_BE_COMPLETE(40, "待完善发货信息"),
