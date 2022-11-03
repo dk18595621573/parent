c7dc9e9b74499de7811a84f2c225c58dc43de1a4
曾浩 <epdcgsi@dingtalk.com>
2022-11-03 10:24:22 +0800

    分页响应对象

--- cloud-webmvc/src/main/java/com/cloud/webmvc/domain/Pageable.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/domain/Pageable.java
@@ -18,7 +18,7 @@ import java.util.List;
 @ApiModel("分页数据")
 public class Pageable<T> implements Serializable {
 
-    private static final Pageable EMPTY_PAGE = new Pageable<>(0, Collections.emptyList());
+    private static final Pageable EMPTY_PAGE = new Pageable<>(0, Collections.emptyList(), null);
 
     @ApiModelProperty("总记录数")
     private long total;
@@ -26,16 +26,24 @@ public class Pageable<T> implements Serializable {
     @ApiModelProperty("列表数据")
     private List<T> rows;
 
+    @ApiModelProperty("每页记录条数")
+    private Integer size;
+
     public Pageable() {
     }
 
-    public Pageable(final long total, final List<T> rows) {
+    public Pageable(final long total, final List<T> rows, final Integer size) {
         this.total = total;
         this.rows = rows;
+        this.size = size;
     }
 
-    public static <T> Pageable<T> of(long total, List<T> rows) {
-        return new Pageable<>(total, rows);
+    public static <T> Pageable<T> of(final long total, final List<T> rows) {
+        return of(total, rows, null);
+    }
+
+    public static <T> Pageable<T> of(final long total, final List<T> rows, final Integer size) {
+        return new Pageable<>(total, rows, size);
     }
 
     public static <T> Pageable<T> empty() {
