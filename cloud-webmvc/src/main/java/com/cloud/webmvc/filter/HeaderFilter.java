a22ed2ed16888f40c4849c45ed7c7407170af801
曾浩 <epdcgsi@dingtalk.com>
2022-12-12 10:01:22 +0800

    链路traceId 调整

--- cloud-webmvc/src/main/java/com/cloud/webmvc/filter/HeaderFilter.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/filter/HeaderFilter.java
@@ -1,6 +1,7 @@
 package com.cloud.webmvc.filter;
 
 import com.cloud.common.constant.Constants;
+import com.cloud.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +23,8 @@ public class HeaderFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws ServletException, IOException {
         try {
-            MDC.put(Constants.MDC_TRACE_ID, String.valueOf(System.currentTimeMillis()));
+            String traceId = request.getHeader(Constants.MDC_TRACE_ID);
+            MDC.put(Constants.MDC_TRACE_ID, StringUtils.defaultString(traceId, String.valueOf(System.currentTimeMillis())));
             chain.doFilter(request, response);
         } finally {
             MDC.clear();
