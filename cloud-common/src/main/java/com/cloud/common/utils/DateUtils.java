289bfb9cddff6ff44097005e1f609e798e91fe1e
曾浩 <epdcgsi@dingtalk.com>
2022-12-14 15:06:24 +0800

    date工具类

--- cloud-common/src/main/java/com/cloud/common/utils/DateUtils.java
+++ cloud-common/src/main/java/com/cloud/common/utils/DateUtils.java
@@ -44,6 +44,10 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
         return new Date();
     }
 
+    public static Date yesterday() {
+        return offsetDay(getNowDate(), -1);
+    }
+
     /**
      * 获取当前日期, 默认格式为yyyy-MM-dd
      *
@@ -199,6 +203,21 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
         time.set(Calendar.MINUTE, 59);
         time.set(Calendar.SECOND, 59);
         time.set(Calendar.MILLISECOND, 0);
+        return time.getTime();
+    }
+
+    public static Date offsetDay(final Date date, int offset) {
+        return offset(date, Calendar.DAY_OF_YEAR, offset);
+    }
+
+    public static Date offsetHour(final Date date, int offset) {
+        return offset(date, Calendar.HOUR_OF_DAY, offset);
+    }
+
+    public static Date offset(final Date date, int field, int offset) {
+        Calendar time = Calendar.getInstance();
+        time.setTime(date);
+        time.add(field, offset);
         return time.getTime();
     }
 
