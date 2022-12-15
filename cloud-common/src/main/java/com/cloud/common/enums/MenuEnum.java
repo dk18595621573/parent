80ba2473c50f6f5b275f1df2a344be94361692ac
Mft <2664370462@qq.com>
2022-12-15 17:30:59 +0800

    权限菜单枚举

---
+++ cloud-common/src/main/java/com/cloud/common/enums/MenuEnum.java
@@ -0,0 +1,50 @@
+package com.cloud.common.enums;
+
+import com.cloud.common.constant.Constants;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+/**
+ * 菜单权限枚举
+ *
+ * @author author
+ */
+public enum MenuEnum {
+    INFO("info", "C", "商户管理"),
+    SUB_ACCOUNT("subAccount", "C", "账号管理"),
+    PAYER("payer", "C", "付款主体"),
+    MANAGEMENT("management", "C", "店铺管理"),
+    COMPANY_MANAGEMENT("companyManagement", "C", "供应商管理"),
+    READONLY("readonly", "R", "只读");
+
+
+    private final String path;
+    private final String type;
+    private final String remark;
+
+    MenuEnum(String path, String type, String remark) {
+        this.path = path;
+        this.type = type;
+        this.remark = remark;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public static String getPaths() {
+        return Arrays.stream(MenuEnum.values()).map(MenuEnum::getPath).collect(Collectors.joining(Constants.ROLE_DELIMETER));
+    }
+
+}
