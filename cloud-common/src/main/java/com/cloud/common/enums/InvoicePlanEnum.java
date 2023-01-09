058024bf49185ff036a3d310f5c9562b8335e582
zhushanshuo <zhushanshuo@nl-ds.cn>
2023-01-09 14:32:52 +0800

    发票计划状态枚举

---
+++ cloud-common/src/main/java/com/cloud/common/enums/InvoicePlanEnum.java
@@ -0,0 +1,36 @@
+package com.cloud.common.enums;
+
+/**
+ * 发票汇总表 状态
+ * @author zhushanshuo
+ * @date 2023.01.09 13:01
+ */
+public enum InvoicePlanEnum implements BaseEnum {
+
+	UN_REACH(10, "未到票"),
+	SOME_REACH(20, "部分到票"),
+	WAIT_CHECK(30, "待核验"),
+	CHECK_SUCCESS(40, "核验成功"),
+	CHECK_FAIL(50, "核验失败"),
+	;
+
+	Integer code;
+
+	String msg;
+
+	InvoicePlanEnum(Integer code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+
+	@Override
+	public Integer getCode() {
+		return code;
+	}
+
+	@Override
+	public String getMsg() {
+		return msg;
+	}
+
+}
