82592baeb5665a8cc34d47663402070a03dd2be1
zhouyong <1198113391@qq.com>
2023-01-29 16:13:02 +0800

    订单归属枚举迁移

---
+++ cloud-common/src/main/java/com/cloud/common/enums/BelongType.java
@@ -0,0 +1,42 @@
+package com.cloud.common.enums;
+
+import java.util.Objects;
+
+/**
+ * 订单归属类型
+ */
+public enum BelongType {
+    DEFAULT("DEFAULT", "默认内部系统"),
+    HUASHENG("HUASHENG","华盛"),
+    OTHERS("OTHERS","其他")
+
+    ;
+
+    private String code;
+
+    private String message;
+
+    BelongType(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static BelongType fromCode(String code) {
+        if (Objects.nonNull(code)) {
+            for (BelongType belongType : BelongType.values()) {
+                if (belongType.getCode().equals(code)) {
+                    return belongType;
+                }
+            }
+        }
+        return null;
+    }
+}
