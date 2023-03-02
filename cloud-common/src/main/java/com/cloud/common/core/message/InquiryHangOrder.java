6ec7c1682242c821a00781f1e4d50940304c07a6
zhouyong <1198113391@qq.com>
2023-03-02 09:57:03 +0800

    询价采购推送

--- cloud-common/src/main/java/com/cloud/common/core/message/InquiryHangOrder.java
+++ cloud-common/src/main/java/com/cloud/common/core/message/InquiryHangOrder.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Data
 @Accessors(chain = true)
 public class InquiryHangOrder implements Serializable {
     /**
-     * 订单主键
+     * 订单主键（父订单）
      */
     private Long id;
 
@@ -17,4 +18,21 @@ public class InquiryHangOrder implements Serializable {
      * 新增出价数量
      */
     private Long addHangQuantity;
+
+    /**
+     * 成交订单ID（子订单）
+     */
+    private Long orderId;
+
+    /**
+     * 成交价格
+     */
+    private BigDecimal price;
+
+    /**
+     * 成交企业名称
+     */
+    private String tradeCompanyName;
+
+
 }
