d47892bed7d9b2084e564bdc6e83d6b2f2b86fd6
罗泽强 <16621668829@163.com>
2023-03-09 14:01:11 +0800

    update：招商银行配置项

--- cloud-component/src/main/java/com/cloud/component/properties/CMBProperties.java
+++ cloud-component/src/main/java/com/cloud/component/properties/CMBProperties.java
@@ -5,23 +5,31 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- *  招商银行配置属性
+ * 招商银行配置属性.
+ *
+ * @author nlsm
+ * @date 2023-3-9 13:46:01
  */
 @Data
 @ConfigurationProperties(prefix = CMBProperties.CMB_PREFIX)
-public class CMBProperties{
+public class CMBProperties {
 
     public static final String CMB_PREFIX = Constants.CONFIG_PREFIX + "cmb";
 
     /**
-     * 用户id
+     * 用户id.
      */
     private String UID;
 
     /**
-     * 前置机访问地址
+     * 前置机访问地址.
      */
     private String URL;
+
+    /**
+     * 业务模式（模式编号）.
+     */
+    private String busMod;
 
     /**
      * 银行公钥.
