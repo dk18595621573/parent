e04009e20094ea4bc6d1d8bba439f0e765d2c07a
罗泽强 <16621668829@163.com>
2023-04-03 16:53:06 +0800

    即时通讯IM

--- cloud-starter/tencent-spring-boot-starter/src/main/java/com/cloud/tencent/model/im/BaseResponse.java
+++ cloud-starter/tencent-spring-boot-starter/src/main/java/com/cloud/tencent/model/im/BaseResponse.java
@@ -16,6 +16,9 @@ import java.io.Serializable;
 @Accessors(chain = true)
 public class BaseResponse implements Serializable {
 
+    public static final int SUCCESS = 0;
+    public static final int ERROR = 1;
+
     private static final long serialVersionUID = 5591572268463163013L;
 
     @JsonProperty("ActionStatus")
@@ -26,6 +29,54 @@ public class BaseResponse implements Serializable {
 
     @JsonProperty("ErrorCode")
     private Integer errorCode;
+
+    /**
+     * 返回成功消息.
+     *
+     * @return 成功消息
+     */
+    public static BaseResponse success() {
+        return BaseResponse.success(SUCCESS, "回调成功");
+    }
+
+    /**
+     * 返回成功消息.
+     *
+     * @param errorCode 错误码
+     * @param errorInfo 错误信息
+     * @return 成功消息
+     */
+    public static BaseResponse success(final Integer errorCode, final String errorInfo) {
+        BaseResponse response = new BaseResponse();
+        response.setErrorCode(errorCode);
+        response.setErrorInfo(errorInfo);
+        response.setActionStatus("OK");
+        return response;
+    }
+
+    /**
+     * 返回失败消息.
+     *
+     * @return 失败消息
+     */
+    public static BaseResponse error() {
+        return BaseResponse.success(ERROR, "回调出错");
+    }
+
+    /**
+     * 返回失败消息.
+     *
+     * @param errorCode 错误码
+     * @param errorInfo 错误信息
+     * @return 失败消息
+     */
+    public static BaseResponse error(final Integer errorCode, final String errorInfo) {
+        BaseResponse response = new BaseResponse();
+        response.setErrorCode(errorCode);
+        response.setErrorInfo(errorInfo);
+        response.setActionStatus("FAIL");
+        return response;
+    }
 
 }
 
