81e8e2c5937fc09c69ca1cb3c4cd119c7e024b2b
曾浩 <epdcgsi@dingtalk.com>
2023-04-18 14:05:45 +0800

    全局异常描述调整

--- cloud-webmvc/src/main/java/com/cloud/webmvc/exception/GlobalExceptionHandler.java
+++ cloud-webmvc/src/main/java/com/cloud/webmvc/exception/GlobalExceptionHandler.java
@@ -92,17 +92,17 @@ public class GlobalExceptionHandler {
         if ("org.apache.dubbo.rpc.RpcException".equals(e.getClass().getName())) {
             return Result.error("服务异常，请稍后再试");
         }
-        return Result.error("操作异常，请联系管理员");
+        return Result.error("系统异常，请联系管理员");
     }
 
     /**
-     * 系统异常
+     * 未知的系统异常
      */
     @ExceptionHandler(Exception.class)
     public Result<?> handleException(Exception e, HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         log.error("请求地址'{}',发生系统异常.", requestURI, e);
-        return Result.error("系统异常，请联系管理员");
+        return Result.error("未知异常，请联系管理员");
     }
 
     /**
