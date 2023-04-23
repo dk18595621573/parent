e859e6c4d5d95b567d6fb5ebd32ddc22040f4634
zhanll <417976745@qq.com>
2023-04-23 10:12:51 +0800

    账单保存mq消息体

---
+++ cloud-starter/rocketmq-spring-boot-starter/src/main/java/com/cloud/rocketmq/consts/FmsMQConstants.java
@@ -0,0 +1,19 @@
+package com.cloud.rocketmq.consts;
+
+/**
+ * fms消息常量
+ * @Author:zhanll
+ * @Date:2023-04-23 9:26
+ */
+public interface FmsMQConstants {
+
+    /**
+     * 模块名
+     */
+    String MODEL_NAME = "FMS";
+
+    /**
+     *
+     */
+    String TAGS_SAVE_BILL = "SAVE_BILL";
+}
--- cloud-starter/rocketmq-spring-boot-starter/src/main/java/com/cloud/rocketmq/message/bill/FmsSaveBillMessage.java
+++ cloud-starter/rocketmq-spring-boot-starter/src/main/java/com/cloud/rocketmq/message/bill/FmsSaveBillMessage.java
@@ -1,12 +1,15 @@
 package com.cloud.rocketmq.message.bill;
 
+import cn.hutool.core.util.StrUtil;
 import com.cloud.rocketmq.base.BaseEvent;
+import com.cloud.rocketmq.consts.FmsMQConstants;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * 账单生成消息体
@@ -31,4 +34,22 @@ public class FmsSaveBillMessage extends BaseEvent {
      * 账单备注
      */
     private String remark;
+
+    @Override
+    public String keys() {
+        String keys = super.keys;
+        if (Objects.nonNull(keys)) {
+            return StrUtil.format("{}-{}-{}", keys, FmsMQConstants.TAGS_SAVE_BILL, this.orderId);
+        }
+        return StrUtil.format("{}-{}-{}", FmsMQConstants.TAGS_SAVE_BILL, System.currentTimeMillis(), this.orderId);
+    }
+
+    @Override
+    public String tags() {
+        String tags = super.tags;
+        if (Objects.nonNull(tags)) {
+            StrUtil.format("{}-{}-{}", tags, FmsMQConstants.MODEL_NAME, FmsMQConstants.TAGS_SAVE_BILL);
+        }
+        return StrUtil.format("{}-{}", FmsMQConstants.MODEL_NAME, FmsMQConstants.TAGS_SAVE_BILL);
+    }
 }
