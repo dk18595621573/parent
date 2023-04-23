2e6f175251fa1e42b9bf61f1f569877b17c3b7ec
lingxiang <15801909724@163.com>
2023-04-23 14:16:20 +0800

    供应商群接龙列表推送去除null字段

--- cloud-common/src/main/java/com/cloud/common/core/message/AgentJointOrder.java
+++ cloud-common/src/main/java/com/cloud/common/core/message/AgentJointOrder.java
@@ -1,5 +1,6 @@
 package com.cloud.common.core.message;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,6 +13,7 @@ import java.util.List;
  * @author m
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AgentJointOrder implements Serializable {
 
     /**
