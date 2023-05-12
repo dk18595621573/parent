3a9d77ec640bb7a1ed94dd9d862dcf02e3eaa0ea
Mft <2664370462@qq.com>
2023-05-12 11:20:24 +0800

    企查查配置

--- cloud-component/src/main/java/com/cloud/component/config/QichachaConfig.java
+++ cloud-component/src/main/java/com/cloud/component/config/QichachaConfig.java
@@ -1,17 +1,21 @@
 package com.cloud.component.config;
 
+import com.cloud.component.properties.CMBProperties;
 import com.cloud.component.properties.QichachaProperties;
 import com.cloud.component.qichacha.QichachaClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 企查查配置
+ *
  * @author m
  */
 @Configuration
 @EnableConfigurationProperties(value = QichachaProperties.class)
+@ConditionalOnProperty(prefix = CMBProperties.CMB_PREFIX, name = "enabled", havingValue = "true")
 public class QichachaConfig {
 
     @Bean
