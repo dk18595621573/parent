a5fdbecf90ab2a27b6defe355cd4301e3d406ca1
罗泽强 <16621668829@163.com>
2023-05-16 18:14:03 +0800

    动态刷新配置

--- cloud-component/src/main/java/com/cloud/component/config/ECSSConfig.java
+++ cloud-component/src/main/java/com/cloud/component/config/ECSSConfig.java
@@ -4,6 +4,7 @@ import com.cloud.component.ecss.ECSSClient;
 import com.cloud.component.properties.ECSSProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +19,14 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(value = ECSSProperties.class)
 public class ECSSConfig {
 
+    /**
+     * 广移客户端.
+     *
+     * @param ecssProperties 广移配置
+     * @return 结果
+     */
     @Bean
+    @RefreshScope
     public ECSSClient ecssClient(ECSSProperties ecssProperties) {
         return new ECSSClient(ecssProperties);
     }
